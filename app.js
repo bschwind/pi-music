f0804b6faa6b0b99f140b4b46b4afb9b3f7a2443
Brian Schwind <brianmschwind@gmail.com>
2014-09-20 00:38:14 -0400

    Add 'formidable' file upload parser

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var lame = require('lame');
 var Speaker = require('speaker');
 var sqlite3 = require('sqlite3').verbose();
+var formidable = require('formidable');
 
 var databaseName = "songs.db";
 
@@ -30,12 +31,25 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/login", function(req, res) {
-	insertStatement.run(req.body.username, function (){
-		res.send(req.body);
+app.post("/uploadsong", function(req, res) {
+	var form = new formidable.IncomingForm();
+
+	form.parse(req, function(err, fields, files) {
+		console.log("Received " + files.song.name);
+		fs.readFile(files.song.path, function (err, data) {
+			if (err) {
+				throw err;
+			}
+
+			// Insert song into the database
+			insertStatement.run(files.song.name, data, function() {
+				res.send(req.body);
+			});
+		});
 	});
 });
 
+// Returns a list of song names in the database
 app.get("/log", function (req, res) {
 	var things = [];
 
@@ -45,6 +59,18 @@ app.get("/log", function (req, res) {
 		},
 		function(err, n) { // Statement completion
 			res.send(things);
+		}
+	);
+})
+
+// Just returns the first file uploaded to the database as a test
+app.get("/firstsong", function (req, res) {
+	db.each("SELECT song_file FROM song where oid = 1",
+		function(err, row) { // Each Row
+			res.send(row.song_file);
+		},
+		function(err, n) { // Statement completion
+
 		}
 	);
 })
